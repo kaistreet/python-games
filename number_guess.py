9b98189a3856eb0c3a7c0189a83d6344f70dc3c4
Kai <mrkaistreet@gmail.com>
2019-09-22 18:08:52 -0400

    Create number_guess.py

---
+++ number_guess.py
@@ -0,0 +1,26 @@
+"""Problem 9:
+Generate a random number between 1 and 9 (including 1 and 9).
+Ask the user to guess the number.
+Then, tell them whether they guessed too low, too high, or exactly right."""
+
+import random
+def randomizer():
+	rando_numbered_list = random.randint(1,9)
+	ask_user_number = int(input('Guess a number between 1 and 9: '))
+	if ask_user_number == rando_numbered_list:
+		print('you guessed correctly! the number was:',rando_numbered_list)
+	elif ask_user_number > rando_numbered_list:
+		print('you guessed too high! the number was:',rando_numbered_list)
+	elif ask_user_number < rando_numbered_list:
+		print('you guessed too low! the number was:',rando_numbered_list)
+	else:
+		print("i don't think you guessed a number")
+
+randomizer()
+play_again = str(input('want to play again? '))
+y = ['y','ya','yee','yes','yup','ok','sure','mhmm','yeah','yea']
+while play_again in y:
+	randomizer()
+	play_again = str(input('want to play again? '))
+else:
+	print('hope you enjoyed this game!')
