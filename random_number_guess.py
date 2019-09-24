b7c92c114c011b9f398c6301dabf16f7d9a3b0ea
Kai <mrkaistreet@gmail.com>
2019-09-24 19:35:25 -0400

    Update random_number_guess.py

--- random_number_guess.py
+++ random_number_guess.py
@@ -1,8 +1,3 @@
-"""Problem 9:
-Generate a random number between 1 and 9 (including 1 and 9).
-Ask the user to guess the number.
-Then, tell them whether they guessed too low, too high, or exactly right."""
-
 import random
 def randomizer():
 	rando_numbered_list = random.randint(1,9)
