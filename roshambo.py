1efd30ad4a01cc867f27fc808bdc9d24f1542e5e
Kai <mrkaistreet@gmail.com>
2019-10-15 21:52:37 -0400

    Update roshambo.py

--- roshambo.py
+++ roshambo.py
@@ -1,36 +1,46 @@
-#A two-player Rock-Paper-Scissors game.
+#Make a two-player Rock-Paper-Scissors game.
+import sys
 print("Let's play ro sham bo!")
-user_one_name = str(input('Ready player one? Tell me your name: '))
-print('Welcome',user_one_name)
-user_two_name = str(input('Ready player two? Tell me your name: '))
-print('Welcome',user_two_name)
-
-def roshambo():
-	rock = ['r','ro','roc','rock','stone','boulder','pebble']
-	paper = ['p','pa','pap','pape','paper','sheet']
-	scissors = ['s','sc','sci','scis','sciss','scisso','scissor','scissors','shears','cut']
-	user_one_rps = str(input('Hey player one! Rock, paper or scissors?'))
-	user_two_rps = str(input('Hey player two! Rock, paper or scissors?'))
-	if ((user_one_rps in rock) and (user_two_rps in scissors)):
-		print('player one wins! sorry player two!')
-	elif ((user_one_rps in paper) and (user_two_rps in rock)):
-		print('player one wins! sorry player two!')
-	elif ((user_one_rps in scissors) and (user_two_rps in paper)):
-		print('player one wins! sorry player two!')
-	elif ((user_two_rps in rock) and (user_one_rps in scissors)):
-		print('player two wins! sorry player one!')
-	elif ((user_two_rps in paper) and (user_one_rps in rock)):
-		print('player two wins! sorry player one!')
-	elif ((user_two_rps in scissors) and (user_one_rps in paper)):
-		print('player two wins! sorry player one!')
-	else:
-		print("you're not playing the game right!")
-
-roshambo()
-play_again = str(input('want to play again?'))
-y = ['y','ya','yes','yup','yea','yeah','yup','mhmm','sure']
-while play_again in y:
-	roshambo()
-	play_again = str(input('want to play again? '))
+player_1 = str(input('Player 1, enter your name: '))
+print('Hi ' + player_1 + '!')
+player_2 = str(input('Player 2, enter your name: '))
+print('Hi ' + player_2 + '!')
+y = ['y','yes','yup','yeah','yea','ya']
+ready_p1 = str(input(player_1 + ', are you ready? '))
+if ready_p1 in y:
+  print('ok!')
+  ready_p2 = str(input(player_2 + ', are you ready? '))
+  if ready_p2 in y:
+    print('ok!')
 else:
-	print('hope you had fun!')
+  print("alright, we won't play then.")
+  sys.exit()
+def roshambo():
+  rock = ['rock','r']
+  paper = ['paper','p']
+  scissors = ['scissors','s']
+  rps_1 = str(input(player_1 + '! Rock, Paper or Scissors? '))
+  rps_2 = str(input(player_2 + '! Rock, Paper or Scissors? '))
+  if ((rps_1 in rock) and (rps_2 in scissors)):
+    print(player_1 + ' wins! sorry ' + player_2)
+  elif ((rps_1 in paper) and (rps_2 in rock)):
+    print(player_1 + ' wins! sorry ' + player_2)
+  elif ((rps_1 in scissors) and (rps_2 in paper)):
+    print(player_1 + ' wins! sorry ' + player_2)
+  elif ((rps_2 in rock) and (rps_1 in scissors)):
+    print(player_2 + ' wins! sorry ' + player_1)
+  elif ((rps_2 in paper) and (rps_1 in rock)):
+    print(player_2 + ' wins! sorry ' + player_1)
+  elif ((rps_2 in scissors) and (rps_1 in paper)):
+    print(player_2 + ' wins! sorry ' + player_1)
+  elif rps_1 == rps_2:
+    print("it's a tie!")
+roshambo()
+play_again = str(input('want to play again? '))
+y = ['y','yes','yup','yeah','yea','ya']
+while play_again in y:
+  roshambo()
+  play_again = str(input('want to play again? '))
+else:
+  print('hope you had fun!')
+  sys.exit()
